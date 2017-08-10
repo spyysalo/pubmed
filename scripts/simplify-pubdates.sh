18e64d9260ba8cab42d32cb608914d0df2869aaa
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2017-08-10 13:42:25 +0100

    Output year only

--- scripts/simplify-pubdates.sh
+++ scripts/simplify-pubdates.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Convert output of extractTIABs.py --json --metadata --no-title --no-abstract
-# into YEAR<TAB>PUBDATE format.
+# into PMID<TAB>YEAR format, where YEAR is the publication year.
 
 for f in "$@"; do
     egrep '^[[:space:]]*"(id|pubdate)":' $f \
 	| perl -pe 's/"//g' \
-	| perl -pe 's/^\s*id:\s*(\d+),?\s*/$1\t/; s/^\s*pubdate:\s*//'
+	| perl -pe 's/^\s*id:\s*(\d+),?\s*/$1\t/; s/^\s*pubdate:\s*//' \
+	| perl -pe 's/\t(?:spring|summer|autumn|fall|winter) (\d{4})/\t$1/i' \
+	| perl -pe 's/(\t[0-9]{4})\b.*/$1/'
 done
