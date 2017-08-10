48c03bb2972732abdb1e680367a59da256d6f473
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2017-08-10 12:07:34 +0100

    Input file names as arguments

--- scripts/simplify-pubdates.sh
+++ scripts/simplify-pubdates.sh
@@ -3,7 +3,8 @@
 # Convert output of extractTIABs.py --json --metadata --no-title --no-abstract
 # into YEAR<TAB>PUBDATE format.
 
-cat medline16n0*.meta \
-    | egrep '^[[:space:]]*"(id|pubdate)":' \
-    | perl -pe 's/"//g' \
-    | perl -pe 's/^\s*id:\s*(\d+),?\s*/$1\t/; s/^\s*pubdate:\s*//'
+for f in "$@"; do
+    egrep '^[[:space:]]*"(id|pubdate)":' $f \
+	| perl -pe 's/"//g' \
+	| perl -pe 's/^\s*id:\s*(\d+),?\s*/$1\t/; s/^\s*pubdate:\s*//'
+done
